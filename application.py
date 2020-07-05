04ed511b14eeb3e98894d7a9d2009c0ab03ae90a
Ubuntu <ubuntu@ip-172-31-18-22.ap-southeast-1.compute.internal>
2020-07-05 11:17:59 +0000

    added clean_string and support for longer strings

--- application.py
+++ application.py
@@ -3,9 +3,27 @@ from wtforms import Form, TextField, TextAreaField, validators, StringField, Sub
 from gensim.models import Word2Vec
 import numpy as np
 
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+def clean_string(input):
+    # Make sure input is a string
+    input = input if type(input) is str else ''
+
+    # Turn everything into lower case
+    input = input.lower().strip()
+
+    # Remove unwanted characters
+    input = re.sub(r"[^a-z\ ]", ' ', input, 0, re.MULTILINE)
+
+    # Remove multiple spaces
+    input = re.sub(r"\ +", ' ', input, 0, re.MULTILINE)
+
+    # Return stripped output
+    input = [word for word in input.split() if word in model.vocab]
+    return input
 
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
@@ -36,8 +54,11 @@ def hello():
 @app.route('/form', methods=['POST'])
 def my_form_post():
     text = request.form['text']
-    vec = model[str(text)]
-    returnvector = np.absolute(np.rint(vec*100))
+    text = clean_string(text)
+    a = np.array([model[word] for word in textin if word in model.vocab])
+    b = np.average(a, axis = 0)
+
+    returnvector = np.absolute(np.rint(b*100))
     return render_template('vectorviz.html', len_v = len(vec), v = repr(returnvector)[7:-17].replace('\n',''))
     
 
