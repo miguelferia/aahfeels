b12855f3f15289d5e39ebc35dd78742190c8b142
miguel <miguelferia@gmail.com>
2020-07-08 15:01:41 +0800

    removed random; based colors on PCA

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from gensim.models import Word2Vec
 import numpy as np
+from sklearn.decomposition import PCA
 import re
 
 
@@ -56,12 +57,27 @@ def hello():
 def my_form_post():
     text = request.form['text']
     text = clean_string(text)
-    a = np.array([model[word] for word in text if word in model.vocab])
-    b = np.average(a, axis = 0)
 
-    returnvector = np.absolute(np.rint(b*100))
-    bgvector = np.rint(b*100)
-    return render_template('vectorviz.html', len_v = len(returnvector), v = repr(returnvector)[7:-17].replace('\n',''), bg = repr(bgvector)[7:-17].replace('\n',''))
+    #star
+    input_vector = np.array([model[word] for word in text if word in model.vocab])
+    input_vector = np.average(input_vector, axis = 0)
+    star_vector = np.absolute(np.rint(input_vector*100))
+
+    #color3
+    color_vector3 = np.vstack((text1a, input_vector))
+    color_vector3 = pca3.fit_transform(color_vector3)[-1]
+    color_vector3 = np.rint(color_vector3*100)
+
+    #color6
+    color_vector6 = np.vstack((text2a, input_vector))
+    color_vector6 = pca6.fit_transform(color_vector6)[-1]
+    color_vector6 = np.rint(color_vector6*100)
+
+    return render_template('vectorviz.html', 
+                            len_v = 100, 
+                            v = repr(star_vector)[7:-17].replace('\n',''), 
+                            bg3 = repr(color_vector6)[7:-17].replace('\n',''),
+                            bg6 = repr(color_vector3)[7:-17].replace('\n',''))
     
 
 if __name__ == "__main__":
@@ -69,4 +85,19 @@ if __name__ == "__main__":
     model_tl = Word2Vec.load("model20191210/model20191210.model") #tagalog
     print('model loaded.')
     model = model_tl.wv
-    app.run(host="0.0.0.0", port=80)
+    pca6 = PCA(n_components=6)
+    pca3 = PCA(n_components=3)
+
+    with open('nakpilessay.txt', 'r') as f:
+        text1 = f.read()
+    text1 = text1.lower()
+    text1 = re.sub(re.compile('[^a-z\ ]'), '', text1).split()
+
+    with open('ninoyessay.txt', 'r') as f:
+        text2 = f.reaad()
+    text2 = text2.lower()
+    text2 = re.sub(re.compile('[^a-z\ ]'), '', text2).split()
+
+    text1a = np.array([model[word] for word in text1 if word in model.vocab])
+    text2a = np.array([model[word] for word in text2 if word in model.vocab])
+    app.run(host="0.0.0.0", port=5000)
